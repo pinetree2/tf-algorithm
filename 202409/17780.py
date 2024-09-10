db734b59c1b56f99193ecf521df755cad365f23c
Haesong Lee <79689822+pinetree2@users.noreply.github.com>
2024-09-10 07:58:51 +0000

    add 17780

--- 202409/17780.py
+++ 202409/17780.py
@@ -8,66 +8,215 @@ horse = [list(map(int,sys.stdin.readline().split())) for _ in range(K)]
 # 행과 열의 정보는 1부터 시작이라서 체스판에 맞추려면 -1 해야함
 # 이동방향은 1,2,3,4 (우 좌 상 하)
 
+
+def white(horse_position, r,c):
+  # 말의 위치 정보를 담는 2차원 리스트 (스택 사용)
+  if not horse_position[r-1][c-1]:  
+    # 해당 위치에 말이 없다면 
+    horse_position[r-1][c-1].append(i)  # 말의 순서를 추가 (스택에 push)
+  else:  # 해당 위치에 말이 이미 있다면
+        horse_position[r-1][c-1].append(i)  # 말의 순서를 추가 (스택에 push)
+  return horse_position
+
+
+def red(horse_position, r,c):
+  # 말이 이미 존재하는지 확인 후 순서 반대로 변경
+  if not horse_position[r-1][c-1]:
+    horse_position[r-1][c-1].append(i)
+
+  else:
+      horse_position[r-1][c-1].append(i)  # 말의 순서를 추가 (스택에 push)
+      # 순서를 반대로 변경
+      horse_position[r-1][c-1].reverse()
+  return horse_position
+
 turn = 0
+# 말의 위치를 저장하는 배열 생성
+horse_position = [[[] for _ in range(N)] for _ in range(N)]  
+game_over = False
 # 게임 시작
-while True:
+while not game_over:
   turn +=1
   if turn > 1000:
+    print(-1)
     break
-
-  # 만약 말이 도저히 4개로 합쳐질 수 없다면 return -1
-  # 이건 또 어케알지요 
-
   
   # 말의 정보를 가져온다.
   for i in range(K):
     r,c,d = horse[i]
-    # 근데 이 말의 위치가 겹칠 경우에는 순서를 어디에 담아야하는지를 모르겠음, 따로 담아야 하나? 근데 그러면그때마다 확인을 해야해? 어카지? NxN 배열에 말의 순서를 담는 배열을 따로 만들어야 하나?
+
+    if d == 1: # 오른쪽, 열+1
+      # d 에 맞게 r,c 값 변경
+      r,c = r,c+1
+      # 지금 여기에 배열 크기를 넘어갔을때의 처리가 없음, blue 와 같이 방향변경만 해주면됨 4,0(N보다커진) / -1,0(0보다 작아진) / 0,4 (N보다 커진/ 0,-1 (0보다 작아진)/
+      if c > N:
+        c = N
+        d = 2
+        horse[i] = [r,c,d]
+        continue
+      if arr[r-1][c-1] == 0:
+        if len(horse_position[r-1][c-1]) == 4:
+          game_over = True
+          break
+        horse_position = white(horse_position,r,c)
+      if arr[r-1][c-1] == 1:
+        if len(horse_position[r-1][c-1]) == 4:
+          game_over = True
+          break
+        horse_position = red(horse_position,r,c)
+
+      if arr[r-1][c-1] == 2 :
+        d = 2
+        r,c = r,c-1
+        if c < 0:
+          c = 0
+          d = 1
+          horse[i] = [r,c,d]
+          continue 
+        if arr[r-1][c-1] == 0:
+          if len(horse_position[r-1][c-1]) == 4:
+            game_over = True
+            break
+          horse_position = white(horse_position,r,c)
+        elif arr[r-1][c-1] == 1:
+          if len(horse_position[r-1][c-1]) == 4:
+            game_over = True
+            break
+          horse_position = red(horse_position,r,c)
+        else:
+          c = c+1
+          horse[i] = [r,c,d]
     
-    # 이동 하려는 칸이 흰색 
-    if arr[r-1][c-1] == 0:
-      # 말의 위치 이동
-      # direction 확인
-      if d == 1:
-        # d 에 맞게 r,c 값 변경
-      if d == 2:
-        # d 에 맞게 r,c 값 변경
-      if d == 3:
-        # d 에 맞게 r,c 값 변경
-      if d == 4:
-        # d 에 맞게 r,c 값 변경
+    if d == 2: # 왼쪽, 열 -1
+      # d 에 맞게 r,c 값 변경
+      r,c = r,c-1
+      if c < 0:
+        c = 0
+        d = 1
+        horse[i] = [r,c,d]
+        continue 
+        
+      if arr[r-1][c-1] == 0:
+        if len(horse_position[r-1][c-1]) == 4:
+          game_over = True
+          break
+        horse_position = white(horse_position,r,c)
 
-      # 말이 이미 존재하는지 확인 후 순서 변경 
-        #체스판을 벗어나려는 경우 방향만 바꿔준다.
+      if arr[r-1][c-1] == 1:
+        if len(horse_position[r-1][c-1]) == 4:
+          game_over = True
+          break
+        horse_position = red(horse_position,r,c)
 
-    # 이동하려는 칸이 빨간색 
-    elif arr[r-1][c-1] == 1:
-      # 말의 위치 이동
-      # direction 확인
-      if d == 1:
-        # d 에 맞게 r,c 값 변경
-      if d == 2:
-        # d 에 맞게 r,c 값 변경
-      if d == 3:
-        # d 에 맞게 r,c 값 변경
-      if d == 4:
-        # d 에 맞게 r,c 값 변경
+      if arr[r-1][c-1] == 2 :
+        d = 1
+        r,c = r,c+1
+        if c > N:
+          c = N
+          d = 2
+          horse[i] = [r,c,d]
+          continue
+        if arr[r-1][c-1] == 0:
+          if len(horse_position[r-1][c-1]) == 4:
+            game_over = True
+            break
+          horse_position = white(horse_position,r,c)
+        elif arr[r-1][c-1] == 1:
+          if len(horse_position[r-1][c-1]) == 4:
+            game_over = True
+            break
+          horse_position = red(horse_position,r,c)
+        else:
+          c = c-1
+          horse[i] = [r,c,d]
+      
+    if d == 3:
+      # d 에 맞게 r,c 값 변경
+      r,c = r-1,c
+      if r < 0:
+        r = 0
+        d = 4
+        horse[i] = [r,c,d]
+        continue 
+      if arr[r-1][c-1] == 0:
+        if len(horse_position[r-1][c-1]) == 4:
+          game_over = True
+          break
+        horse_position = white(horse_position,r,c)
 
-      # 말이 이미 존재하는지 확인 후 순서 반대로 변경 
-        #체스판을 벗어나려는 경우 방향만 바꿔준다.
+      if arr[r-1][c-1] == 1:
+        if len(horse_position[r-1][c-1]) == 4:
+          game_over = True
+          break
+        horse_position = red(horse_position,r,c)
 
-    # 이동 하려는 칸이 파란색 
-    elif arr[r-1][c-1] == 2:
-      # 말의 위치 이동
-      # direction 확인 후 방향을 반대로 변경 
-      # 파란색은 방향변경만 하고 이동은 다른 곳에서 할 수 없나?
-      # direction에 맞게 이동
-        # 만약 이때 이동하려는 칸이 또 파란색이라면 방향만 또 변경
+      if arr[r-1][c-1] == 2 :
+        d = 4
+        r,c = r+1,c
+        if r > N:
+          r = N
+          d = 3
+          horse[i] = [r,c,d]
+          continue
+          
+        if arr[r-1][c-1] == 0:
+          if len(horse_position[r-1][c-1]) == 4:
+            game_over = True
+            break
+          horse_position = white(horse_position,r,c)
+        elif arr[r-1][c-1] == 1:
+          if len(horse_position[r-1][c-1]) == 4:
+            game_over = True
+            break
+          horse_position = red(horse_position,r,c)
+        else:
+          # 방향만 바꾸고 이동 x 
+          r = r-1
+          horse[i] = [r,c,d]
 
-      #체스판을 벗어나려는 경우 방향만 바꿔준다.
-      
-      
-    
-      
-      
-      
+    if d == 4:
+      # d 에 맞게 r,c 값 변경
+      r,c= r+1,c
+      if r > N:
+        r = N
+        d = 3
+        horse[i] = [r,c,d]
+        continue
+        
+      if arr[r-1][c-1] == 0:
+        if len(horse_position[r-1][c-1]) == 4:
+          game_over = True
+          break
+        horse_position = white(horse_position,r,c)
+
+      if arr[r-1][c-1] == 1:
+        if len(horse_position[r-1][c-1]) == 4:
+          game_over = True
+          break
+        horse_position = red(horse_position,r,c)
+
+      if arr[r-1][c-1] == 2 :
+        d =3
+        r,c = r-1,c
+        if r < 0:
+          r = 0
+          d = 4
+          horse[i] = [r,c,d]
+          continue 
+        if arr[r-1][c-1] == 0:
+          if len(horse_position[r-1][c-1]) == 4:
+            game_over = True
+            break
+          horse_position = white(horse_position,r,c)
+        elif arr[r-1][c-1] == 1:
+          if len(horse_position[r-1][c-1]) == 4:
+            game_over = True
+            break
+          horse_position = red(horse_position,r,c)
+        else:
+          r = r+1
+          horse[i] = [r,c,d]
+  if game_over:
+    print(turn)
+    break
+  
