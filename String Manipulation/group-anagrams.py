b56275dc55f99826516a18735bbb4136a44fced3
pinetree2 <pinetree3966@gmail.com>
2025-03-12 20:10:25 +0900

    add String

---
+++ String Manipulation/group-anagrams.py
@@ -0,0 +1,6 @@
+def groupAnagrams(self,strs: List[str]) -> List[List[str]]:
+    anagrams = collections.defaultdict(list)
+
+    for word in strs:
+        anagrams[''.join(sorted(word))].append(word)
+    return anagrams.values()
---
+++ String Manipulation/valid-palindrome2.py
@@ -0,0 +1,4 @@
+def isPalindrome(self,s:str) -> bool:
+    s = s.lower()
+    s = re.sub('[^a-z0-9]','',s)
+    return s == s[::-1]
